73d1d3fcf7c2f3013adca481934d0b3319f1f172
KConlon13 <KyleConlon13@gmail.com>
2019-10-01 11:15:24 -0400

    deploy

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Mod5AllergyProjectBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Rails.application.config.middleware.insert_before 0, Rack::Cors do
     #   allow do
